6d5ca9dad9d9c04b41cb0175d9448119d3d67388
Shalkar <93659536+AxeSelecao@users.noreply.github.com>
2022-12-23 23:16:33 +0600

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+export const Footer = () => {
+  return <footer className="footer container">
+	<p style={{fontFamily: 'Nothing You Could Do, cursive', fontSize: '30px', textAlign: 'center'}}>Made by Shalkar Koshenayev <br /> 2022</p>
+  </footer>;
+};
+ 
